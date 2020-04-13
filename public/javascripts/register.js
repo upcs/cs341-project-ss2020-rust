85a4f661e0e27e1fb6a082dc158bccdca68582f9
kamala21 <42782295+kamala21@users.noreply.github.com>
2020-04-13 14:54:14 -0700

    register modal completed

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -13,34 +13,61 @@ function register() {
     var lastName = document.getElementById('signupLastName').value;
     var email = document.getElementById('signupEmail').value;
     var password = document.getElementById('signupPassword').value;
+    var confirmPassword = document.getElementById('signupConfirmPassword').value;
     var validLogin = true;
     if (!validUsername(username)) {
+        document.getElementById('usernameError').style.display = 'block';
         validLogin = false;
+    } else{
+        document.getElementById('usernameError').style.display = 'none';
     }
     if (!validFirstName(firstName)) {
+        document.getElementById('firstNameError').style.display = 'block';
         validLogin = false;
+    } else{
+        document.getElementById('firstNameError').style.display = 'none';
     }
     if (!validLastName(lastName)) {
+        document.getElementById('lastNameError').style.display = 'block';
         validLogin = false;
+    } else{
+        document.getElementById('lastNameError').style.display = 'none';
     }
     if (!validEmail(email)) {
+        document.getElementById('emailError').style.display = 'block';
         validLogin = false;
+    } else{
+        document.getElementById('emailError').style.display = 'none';
     }
     if (!validPassword(password)) {
+        document.getElementById('passwordError').style.display = 'block';
         validLogin = false;
+    } else{
+        document.getElementById('passwordError').style.display = 'none';
+    }
+    if (password != confirmPassword){
+        document.getElementById('confirmPasswordError').style.display = 'block';
+        validLogin = false;
+    } else{
+        document.getElementById('confirmPasswordError').style.display = 'none';
     }
     if (validLogin) {
         $.post("/register?username=" + username + '&password=' + password + '&firstName=' + firstName + '&lastName=' + lastName + '&email=' + email, function (data) {
             if (data == "success") {
+                document.getElementById('usernameExists').style.display = 'none';
+                document.getElementById('emailExists').style.display = 'none';
                 document.getElementById('id02').style.display = 'none';
                 localStorage.setItem('username', username);
                 window.location.href = "indexUser.html";
             } else if (data == "usernameFound=trueemailFound=false") {
-                alert("username already exists");
+                document.getElementById('usernameExists').style.display = 'block';
+                document.getElementById('emailExists').style.display = 'none';
             } else if (data == "usernameFound=falseemailFound=true") {
-                alert("email already exists");
+                document.getElementById('usernameExists').style.display = 'none';
+                document.getElementById('emailExists').style.display = 'block';
             } else {
-                alert("username and email already exist");
+                document.getElementById('usernameExists').style.display = 'block';
+                document.getElementById('emailExists').style.display = 'block';
             }
 
         }); //creates user in database logs them in and brings them to homepage
@@ -51,11 +78,10 @@ function register() {
 
 /** BELOW ARE VALIDATION FUNCTIONS TO MAKE SURE THAT USER INPUT IS VALID */
 function validUsername(a) {
-    if (a.length < 4) {
-        alert("minimum username length of 4");
-        return false;
+    if (a.length > 3) {
+        return true;
     }
-    return true;
+    return false;
 }
 function validFirstName(a) {
     var num = /^[A-Z][a-z]*$/;
@@ -85,11 +111,11 @@ function validEmail(a) {
     return false;
 }
 function validPassword(a) {
-    if (a.length < 8) {
-        alert("minimum password length of 8");
-        return false;
+    var password = /^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/
+    if (password.test(a)) {
+        return true;
     }
-    return true;
+    return false;
 }
 // end of register.js
 
